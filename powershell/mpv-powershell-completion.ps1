992406ebe0238c4084df335d85a6bc1919a6b25b
stax76 <frank.skare.de@gmail.com>
2023-11-25 15:02:06 +0100

    mpv-powershell-completion bug fix

--- powershell/mpv-powershell-completion.ps1
+++ powershell/mpv-powershell-completion.ps1
@@ -1,40 +1,22 @@
+#!/bin/pwsh
 
-# PowerShell command line completion for the mpv media player.
+# PowerShell command line completion for the mpv/mpv.net media player.
 
-# It can be installed by dot sourcing it in the profile.
+# It can be installed by dot sourcing it in the PowerShell profile.
 
-# Mit license.
-
-$UpdatedOptions = New-Object Collections.Generic.List[Object]
 $Options = New-Object Collections.Generic.List[Object]
-
-Function Get-MD5Hash($inputString) {
-    $MD5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-    $UTF8 = New-Object -TypeName System.Text.UTF8Encoding
-    [BitConverter]::ToString($MD5.ComputeHash($UTF8.GetBytes($inputString))) -replace '-',''
-}
+$UpdateOptions = @('vo', 'ao', 'profile', 'hwdec', 'audio-device', 'scale', 'tscale',
+                   'error-diffusion', 'vulkan-device')
 
 Function SetOptions
 {
     try
     {
-        $version = mpv --version
+        $optionContent = mpv --no-config --list-options
     }
     catch
     {
         throw
-    }
-
-    $optionPath = Join-Path ([IO.Path]::GetTempPath()) ((Get-MD5Hash $version) + ".txt")
-
-    if (Test-Path $optionPath)
-    {
-        $optionContent = Get-Content -Path $optionPath
-    }
-    else
-    {
-        $optionContent = mpv --no-config --list-options
-        $optionContent | Out-File -FilePath $optionPath
     }
 
     foreach ($line in $optionContent)
@@ -46,7 +28,7 @@ Function SetOptions
             continue
         }
 
-        $table = @{ value = ""; type = ""; choices = @() }
+        $table = @{ value = ""; type = ""; choices = $null }
 
         if ($line.Contains(" "))
         {
@@ -71,10 +53,6 @@ Function SetOptions
             $table["choices"] = $value.Substring(8).TrimStart() -split " "
         }
 
-        switch ($table["name"]) {
-            'vo' { $table["type"] = "choice"; $table["choices"] = @('gpu', 'gpu-next', 'direct3d') }
-        }
-
         if ($value.StartsWith('Flag'))
         {
             $table['type'] = 'flag'
@@ -89,35 +67,11 @@ Function SetOptions
         {
             $noTable = @{ name = 'no-' + $table['name'];
                           value = $table['value'];
-                          type = $table['type'];
-                          choices = @()
+                          type = '';
+                          choices = $null
                         }
-            $noTable2 = @{ name = $table['name'] + '=no';
-                           value = $table['value'];
-                           type = $table['type'];
-                           choices = @()
-                         }
-            $yesTable = @{ name = $table['name'] + '=yes';
-                           value = $table['value'];
-                           type = $table['type'];
-                           choices = @()
-                         }
             $Options.Add($table)
             $Options.Add($noTable)
-            $Options.Add($yesTable)
-            $Options.Add($noTable2)
-        }
-        elseif ($table['type'] -eq 'choice')
-        {
-            foreach ($it in $table["choices"])
-            {
-                $choiceTable = @{ name = $table['name'] + '=' + $it;
-                                  value = $table['value'];
-                                  type = $table['type'];
-                                  choices = @()
-                                }
-                $Options.Add($choiceTable)
-            }
         }
         else
         {
@@ -128,127 +82,58 @@ Function SetOptions
 
 Function Update-Option($name)
 {
-    $newOptions = New-Object Collections.Generic.List[Object]
-
     foreach ($it in $Options)
     {
-        $newOptions.Add($it)
+        if ($name -eq $it.name)
+        {
+            $option = $it
+            break
+        }
     }
 
-    foreach ($it in $newOptions)
+    if ($null -eq $option)
     {
-        if ($it['name'] -eq 'ao' -and -not $UpdatedOptions.Contains('ao'))
+        Write-Error "Option $name is unknown."
+        return
+    }
+
+    if ($null -ne $option.choices)
+    {
+        return
+    }
+
+    $dynamicOptions = @(
+        @{ name = 'vulkan-device'; pattern = "^\s*('.+?')" },
+        @{ name = 'audio-device'; pattern = "^\s*('\S+')" },
+        @{ name = 'hwdec'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'error-diffusion'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'tscale'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'scale'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'profile'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'ao'; pattern = '^\s*([-\w]+)' },
+        @{ name = 'vo'; pattern = '^\s*([-\w]+)' }
+     )
+
+    foreach ($opt in $dynamicOptions)
+    {
+        if ($name -eq $opt.name)
         {
-            $output = mpv --ao=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value }
-            $output += @('help')
+            $output = mpv ('--' + $opt.name + '=help') | Select-Object -Skip 1 |
+                 Select-String ($opt.pattern) -AllMatches |
+                 ForEach-Object { $_.matches.Groups[1].Value } |
+                 Select-Object -Unique | Sort-Object
 
-            foreach ($ao in $output)
-            {
-                $Options.Add(@{ name = "ao=" + $ao; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('ao')
-        }
-
-        if ($it['name'] -eq 'profile' -and -not $UpdatedOptions.Contains('profile'))
-        {
-            $output = mpv --profile=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value }
-            $output += @('help')
-
-            foreach ($profile in $output)
-            {
-                $Options.Add(@{ name = "profile=" + $profile; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('profile')
-        }
-
-        if ($it['name'] -eq 'scale' -and -not $UpdatedOptions.Contains('scale'))
-        {
-            $output = mpv --scale=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value } | sort
-            $output += @('help')
-
-            foreach ($scale in $output)
-            {
-                $Options.Add(@{ name = "scale=" + $scale; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('scale')
-        }
-
-        if ($it['name'] -eq 'tscale' -and -not $UpdatedOptions.Contains('tscale'))
-        {
-            $output = mpv --tscale=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value } | sort
-            $output += @('help')
-
-            foreach ($tscale in $output)
-            {
-                $Options.Add(@{ name = "tscale=" + $tscale; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('tscale')
-        }
-
-        if ($it['name'] -eq 'error-diffusion' -and -not $UpdatedOptions.Contains('error-diffusion'))
-        {
-            $output = mpv --error-diffusion=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value } | sort
-            $output += @('help')
-
-            foreach ($errorDiffusion in $output)
-            {
-                $Options.Add(@{ name = "error-diffusion=" + $errorDiffusion; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('error-diffusion')
-        }
-
-        if ($it['name'] -eq 'hwdec' -and -not $UpdatedOptions.Contains('hwdec'))
-        {
-            $output = mpv --hwdec=help | select -skip 1 | sls '^\s*([-\w]+)' -AllMatches |
-                foreach { $_.matches.Groups[1].Value }
-            $output += @('help')
-            $output = $output | select -unique | sort
-
-            foreach ($hwdec in $output)
-            {
-                $Options.Add(@{ name = "hwdec=" + $hwdec; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('hwdec')
-        }
-
-        if ($it['name'] -eq 'audio-device' -and -not $UpdatedOptions.Contains('audio-device'))
-        {
-            $output = mpv --audio-device=help | select -skip 1 | sls "^\s*('\S+')" -AllMatches |
-                foreach { $_.matches.Groups[1].Value }
-            $output = $output | foreach { if ($_ -match "'\w+'") { $_ -replace "'", '' } else { $_ } }
-            $output += @('help')
-
-            foreach ($audioDevice in $output)
-            {
-                $Options.Add(@{ name = "audio-device=" + $audioDevice; value = ""; type = ""; choices = @() })
-            }
-
-            $UpdatedOptions.Add('audio-device')
-        }
-
-        if ($it['name'] -eq 'vulkan-device' -and -not $UpdatedOptions.Contains('vulkan-device'))
-        {
-            $output = mpv --vulkan-device=help | select -skip 1 | sls "^\s*('.+?')" -AllMatches |
-                foreach { $_.matches.Groups[1].Value }
             $output = $output | foreach { if ($_ -match "'\w+'") { $_ -replace "'", '' } else { $_ } }
 
-            foreach ($vulkanDevice in $output)
+            if ($output -is [string])
             {
-                $Options.Add(@{ name = "vulkan-device=" + $vulkanDevice; value = ""; type = ""; choices = @() })
+                $output = @($output)
             }
 
-            $UpdatedOptions.Add('vulkan-device')
+            $output += @('help')
+            $option.choices = $output
+            $option.type = 'choice'
+            break
         }
     }
 }
@@ -260,59 +145,116 @@ Function Get-Completion($cursorPosition, $wordToComplete, $commandName)
         SetOptions
     }
 
-    if (-not $wordToComplete.Contains(' ') -or -not $wordToComplete.ToLower().Contains('mpv'))
-    {
-        return
-    }
-
     if ($commandName.StartsWith('--'))
     {
-        if ($commandName -eq '--ao=') { Update-Option 'ao' }
-        if ($commandName -eq '--profile=') { Update-Option 'profile' }
-        if ($commandName -eq '--hwdec=') { Update-Option 'hwdec' }
-        if ($commandName -eq '--audio-device=') { Update-Option 'audio-device' }
-        if ($commandName -eq '--scale=') { Update-Option 'scale' }
-        if ($commandName -eq '--tscale=') { Update-Option 'tscale' }
-        if ($commandName -eq '--error-diffusion=') { Update-Option 'error-diffusion' }
-        if ($commandName -eq '--vulkan-device=') { Update-Option 'vulkan-device' }
-
         if ($commandName -like '--*-file*=')
         {
-            return (gci -file).FullName | Resolve-Path -Relative |
-                foreach { if ($_.Contains(' ')) { $commandName + "'$_'" } else { $commandName + $_ } }
+            return (Get-ChildItem -file).FullName | Resolve-Path -Relative |
+                ForEach-Object { if ($_.Contains(' ')) { $commandName + "'$_'" } else { $commandName + $_ } }
         }
 
         if ($commandName -match '(--.+-file.*=)(.+)')
         {
-            return (gci -file).FullName | Resolve-Path -Relative |
-                where { $_.ToLower().Contains($Matches[2].ToLower()) } |
-                foreach { if ($_.Contains(' ')) { $Matches[1] + "'$_'" } else { $Matches[1] + $_ } }
+            return (Get-ChildItem -file).FullName | Resolve-Path -Relative |
+                Where-Object { $_.ToLower().Contains($Matches[2].ToLower()) } |
+                ForEach-Object { if ($_.Contains(' ')) { $Matches[1] + "'$_'" } else { $Matches[1] + $_ } }
         }
 
-        return $Options | where { ('--' + $_.name).Contains($commandName) } | foreach { '--' + $_.name }
+        $shortCommandName = $commandName.Substring(2)
+
+        $argName = ''
+
+        if ($commandName.EndsWith('='))
+        {
+            $shortCommandName = $shortCommandName.Substring(0, $shortCommandName.Length -1)
+        }
+        elseif ($commandName.Contains('='))
+        {
+            $shortCommandName = $shortCommandName.Substring(0, $shortCommandName.IndexOf('='))
+            $argName = $commandName.Substring($commandName.IndexOf('=') + 1)
+        }
+
+        foreach ($it in $UpdateOptions)
+        {
+            if ($shortCommandName -eq $it)
+            {
+                Update-Option $it
+                break
+            }
+        }
+
+        $results = New-Object Collections.Generic.List[Object]
+
+        $exactMatches = $Options | Where-Object { $_.name -eq $shortCommandName }
+
+        foreach ($it in $exactMatches)
+        {
+            if (-not $commandName.Contains('='))
+            {
+                continue
+            }
+
+            $arguments = $null
+
+            if ($it.type -eq 'flag')
+            {
+                $arguments = 'yes', 'no'
+            }
+
+            if ($it.type -eq 'choice' -and $null -ne $it.choices)
+            {
+                $arguments = $it.choices
+            }
+
+            if ($null -ne $arguments)
+            {
+                foreach ($arg in $arguments)
+                {
+                    if ($argName -ne '')
+                    {
+                        if ($arg.StartsWith($argName))
+                        {
+                            $results.Add('--' + $it.name + '=' + $arg)
+                        }
+                    }
+                    else
+                    {
+                        $results.Add('--' + $it.name + '=' + $arg)
+                    }
+                }
+            }
+        }
+
+        if (-not $commandName.Contains('='))
+        {
+            $partlyMatches = $Options | Where-Object { $_.name.StartsWith($shortCommandName) }
+
+            foreach ($it in $partlyMatches)
+            {
+                if ($it.name -eq $shortCommandName)
+                {
+                    continue
+                }
+
+                $results.Add('--' + $it.name)
+            }
+        }
+
+        return $results
     }
     elseif ($commandName -eq '')
     {
-        return (gci).FullName | Resolve-Path -Relative
+        return (Get-ChildItem).FullName | Resolve-Path -Relative
     }
     else
     {
-        return (gci).FullName | Resolve-Path -Relative |
-            where { $_.ToLower().Contains($commandName.ToLower()) }
+        return (Get-ChildItem).FullName | Resolve-Path -Relative |
+            Where-Object { $_.ToLower().Contains($commandName.ToLower()) }
     }
 }
 
-# commandName: --ao=
-# wordToComplete: mpv --ao=
-# cursorPosition: 9
-
-# Get-Completion 9 'mpv --ao=' '--ao='
-# Get-Completion 16 'mpv --test-file=' '--test-file='
-
 Register-ArgumentCompleter -Native -CommandName mpv -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
-
-    Add-Content -Path E:\Desktop\completer.txt -Value ("commandName: $commandName`nwordToComplete: $wordToComplete`ncursorPosition: $cursorPosition`n") | Out-Null
 
     Get-Completion $cursorPosition "$wordToComplete" "$commandName" | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
