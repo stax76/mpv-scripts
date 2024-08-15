ed73c23239b6d364d1f7d152bebb4becb79d6d04
stax76 <frank.skare.de@gmail.com>
2024-08-15 12:16:41 +0200

    command-palette.lua: font scaling fix

--- command_palette.lua
+++ command_palette.lua
@@ -13,7 +13,7 @@ local o = {
     menu_x_padding = 5,
     menu_y_padding = 2,
     font_size = 16,
-    scale_with_window = false,
+    scale_with_osd_size = false,
     use_mediainfo = false, -- use MediaInfo CLI tool for track info
 }
 
@@ -438,23 +438,20 @@ mp.register_script_message("show-command-palette", function (name)
     menu.filter_by_fields = { "content" }
     em.get_line = original_get_line_func
 
-    local display_hidpi_scale = mp.get_property_native("display-hidpi-scale")
-
-    if o.scale_with_window then
-        local font_size = math.floor(initial_font_size * display_hidpi_scale)
+    if o.scale_with_osd_size then
+        local font_size = 40
         local width = mp.get_property_native("osd-width")
         local height = mp.get_property_native("osd-height")
-        local display_width = mp.get_property_native("display-width")
 
         if width > height then
-            font_size = math.floor(font_size * width / display_width)
+            font_size = math.floor(font_size * width / 1920)
         else
-            font_size = math.floor(font_size * height / display_width)
+            font_size = math.floor(font_size * height / 1920)
         end
 
         o.font_size = font_size
     else
-        o.font_size = math.floor(initial_font_size * display_hidpi_scale)
+        o.font_size = math.floor(initial_font_size * mp.get_property_native("display-hidpi-scale"))
     end
 
     if name == "Command Palette" then
